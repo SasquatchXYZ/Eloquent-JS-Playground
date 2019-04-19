1a4e8c1909d7c021b2497c012dc3d3e0da756627
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-04-19 15:55:42 -0400

    WIP Chapter 4

--- third-edition/C4/exercises/array-reverse.js
+++ third-edition/C4/exercises/array-reverse.js
@@ -1,9 +1,16 @@
+// My Solution
 const reverseArray = array => {
-
+  const newArray = [];
+  for (let element of array) newArray.unshift(element);
+  return newArray
 };
 
 const reverseArrayInPlace = array => {
-
+  for (let k = array.length - 2, l = array.length - 1; k > l; k--) {
+    array.unshift(array[k]);
+    array.push(array[k]);
+  }
+  return array
 };
 
 console.log(reverseArray(["A", "B", "C"]));
@@ -12,3 +19,29 @@ let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+/*
+// Book Solution
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]*/
